400c30a5d8a8731a506f744831042e064fe5d03f
evermake <vladeryabkin@gmail.com>
2023-10-31 15:38:29 +0300

    style: format file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,30 @@
-"use client";
+"use client"
 
-import React from "react";
-import "./globals.css";
-import { Fredoka } from "next/font/google";
-import RootStyleRegistry from "@/app/emotion";
-import styled from "@emotion/styled";
-import { MetaMaskProvider } from "metamask-react";
-import Footer from "@/components/Footer";
-import { ToastContainer } from "react-toastify";
-import { Web3Provider } from "@/app/hooks/useWeb3";
-import "react-toastify/dist/ReactToastify.css";
-import Header from "@/components/Header";
-import { ProgressLoaderProvider } from "@/components/ProgressLoader/ProgressLoader";
+import React from "react"
+import { Fredoka } from "next/font/google"
+import styled from "@emotion/styled"
+import { MetaMaskProvider } from "metamask-react"
+import { ToastContainer } from "react-toastify"
+import Footer from "@/components/Footer"
+import RootStyleRegistry from "@/app/emotion"
+import { Web3Provider } from "@/app/hooks/useWeb3"
+import Header from "@/components/Header"
+import { ProgressLoaderProvider } from "@/components/ProgressLoader/ProgressLoader"
+
+import "./globals.css"
+import "react-toastify/dist/ReactToastify.css"
 
 const fredoka = Fredoka({
   subsets: ["latin"],
   weight: ["300", "400", "700"],
   variable: "--font-fredoka",
-});
+})
 
 const BodyStyled = styled.body`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-`;
+`
 
 const MainStyled = styled.main`
   display: flex;
@@ -31,12 +32,12 @@ const MainStyled = styled.main`
   justify-content: flex-start;
   align-items: center;
   flex-grow: 1;
-`;
+`
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
     <html lang="en" className={fredoka.variable}>
@@ -61,5 +62,5 @@ export default function RootLayout({
         </Web3Provider>
       </ProgressLoaderProvider>
     </html>
-  );
+  )
 }
