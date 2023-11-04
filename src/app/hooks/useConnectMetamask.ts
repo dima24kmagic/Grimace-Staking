1705b988030f85137fe6a13452324d1531e7fe6f
shoorek <sh00r3k@gmail.com>
2023-11-04 16:29:08 +0600

    feat: fixed button flickering

--- src/app/hooks/useConnectMetamask.ts
+++ src/app/hooks/useConnectMetamask.ts
@@ -1,8 +1,10 @@
 import { useMetaMask } from "metamask-react";
 
 export const useConnectMetamask = () => {
-  const { connect } = useMetaMask();
+  const { connect, account } = useMetaMask();
   const handleConnect = async () => {
+    if(account) return;
+
     await connect().catch(() => {
       return alert("Please connect metamask to continue using application");
     });
