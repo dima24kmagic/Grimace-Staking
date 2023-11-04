1ddb1701c2cb21aa61870e06abbeacea6da26bb6
shoorek <sh00r3k@gmail.com>
2023-11-04 16:17:27 +0600

    feat: connect from init form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import BitGetBanner from "@/components/banners/BitGet"
 import GrimaceSwapBanner from "@/components/banners/GrimaceSwap"
 import Container from "@/components/Container"
 import FormInitial from "@/components/forms/FormInitial"
+import { useConnectMetamask } from "@/app/hooks/useConnectMetamask"
 
 const ContainerStyled = styled(Container)`
   width: 600px;
@@ -15,9 +16,11 @@ const ContainerStyled = styled(Container)`
 `
 
 export default function Home() {
+  const { handleConnect } = useConnectMetamask()
+
   return (
     <ContainerStyled>
-      <FormInitial />
+      <FormInitial onConnectClick={handleConnect} />
       <BitGetBanner />
       <GrimaceSwapBanner />
     </ContainerStyled>
