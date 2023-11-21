0bf9f7e04ea6ba5576388c09ee79b9a5c103f627
evermake <vladeryabkin@gmail.com>
2023-11-21 19:55:35 +0300

    feat: add "Rank By" select

--- src/app/rank/page.tsx
+++ src/app/rank/page.tsx
@@ -1,20 +1,31 @@
 "use client"
 
 import React, { useState } from "react"
+import { Menu } from "@headlessui/react"
+import clsx from "clsx"
 import Page from "@/components/Page"
 import StakingTable from "@/components/StakingTable"
+import ChevronBottomIcon from "@/components/icons/ChevronBottomIcon"
 
 export default () => {
   const [pagesTotal, _] = useState(10)
   const [currentPage, setCurrentPage] = useState(1)
+  const [rankBy, setRankBy] = useState<"amount" | "date">("amount")
 
   return (
     <Page
       heading="Leaderboard"
       subheading={(
-        <span className="text-lg uppercase">
-          Rank by: amount
-        </span>
+        <div className="flex items-center justify-between gap-2">
+          <span className="text-lg uppercase">
+            {"Rank by: "}
+          </span>
+          <RankingSelect
+            options={["amount", "date"]}
+            selected={rankBy}
+            onSelect={option => setRankBy(option as any)}
+          />
+        </div>
       )}
     >
       <div className="max-w-[min(1000px,100%)] mx-auto">
@@ -35,5 +46,45 @@ export default () => {
         />
       </div>
     </Page>
+  )
+}
+
+type RankingSelectProps = {
+  selected: string
+  options: string[]
+  onSelect: (newOption: string) => void
+}
+
+const RankingSelect: React.FC<RankingSelectProps> = ({
+  selected,
+  options,
+  onSelect,
+}) => {
+  return (
+    <Menu as="div" className="relative">
+      <Menu.Button className="uppercase text-lg">
+        {selected}
+        {" "}
+        <ChevronBottomIcon />
+      </Menu.Button>
+      <Menu.Items className="flex flex-col gap-2 absolute right-0 bottom-0 translate-y-[100%] bg-dark rounded-md drop-shadow-lg outline-none p-2 border-2 border-solid border-hint">
+        {options.map(option => (
+          <Menu.Item key={option}>
+            {({ active }) => (
+              <a
+                href="#"
+                onClick={() => onSelect(option)}
+                className={clsx(
+                  "text-lg leading-none",
+                  active && "text-purple",
+                )}
+              >
+                {option}
+              </a>
+            )}
+          </Menu.Item>
+        ))}
+      </Menu.Items>
+    </Menu>
   )
 }
